eba124535b72aa0432b59c22c5e34278ee7827ef
Tatiana <tatianacalderon10@gmail.com>
2023-06-07 07:34:48 -0500

    se modificaron archivos composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'apolm01-033/ejercicio-aplicacion',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '0ce57beabf6d2c0d552b3f59df58d39f8f148c9a',
+        'reference' => 'bfaf6247f460bee83917a6b001894e4f4ad88a64',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'apolm01-033/ejercicio-aplicacion' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '0ce57beabf6d2c0d552b3f59df58d39f8f148c9a',
+            'reference' => 'bfaf6247f460bee83917a6b001894e4f4ad88a64',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
